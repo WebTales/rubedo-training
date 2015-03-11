506b2a98c73793b73b1be71e33514bcfe8a57134
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-03-11 12:08:32 +0100

    theme and block overrides

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,20 @@
 <?php
 return array(
+    /**
+     * Template definitions
+     */
 
+    'templates' => array(
+        'themes' => array(
+            'training' => array(
+                'label' => 'Training',
+                'basePath' => realpath(__DIR__ . '/../theme/training'),
+                'css' => array(
+                    '/css/bootstrap.css',
+                ),
+                'js' => array(
+                ),
+            ),
+        ),
+    ),
 );
