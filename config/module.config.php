d7a4c14450cd24e4bdec15c8cf0722ebe275eb94
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-07-22 14:54:11 +0200

    theme basis

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,19 @@
 <?php
 return array(
-
+    /**
+     * Template definitions
+     */
+    'templates' => array(
+        'themes' => array(
+            'training' => array(
+                'label' => 'Training',
+                'basePath' => realpath(__DIR__ . '/../theme/training'),
+                'css' => array(
+                    '/css/bootstrap.css',
+                ),
+                'js' => array(
+                ),
+            ),
+        ),
+    ),
 );
