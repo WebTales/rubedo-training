08d3270481bf75af99e912ba3daa3ae5f39b023f
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-07-23 12:45:53 +0200

    block

--- theme/training/js/training.js
+++ theme/training/js/training.js
@@ -2,3 +2,11 @@ blocksConfig.trainingComments={
     "template": "/templates/blocks/trainingComments.html",
     "internalDependencies":["/src/modules/rubedoBlocks/controllers/TrainingCommentsController.js"]
 };
+
+angular.module("rubedoDataAccess").factory('CommentsService', ['$http',function($http) {
+    var serviceInstance={};
+    serviceInstance.getComments=function(){
+        return ($http.get("api/v1/comments"));
+    };
+    return serviceInstance;
+}]);
--- theme/training/src/modules/rubedoBlocks/controllers/TrainingCommentsController.js
+++ theme/training/src/modules/rubedoBlocks/controllers/TrainingCommentsController.js
@@ -1,5 +1,14 @@
-angular.module("rubedoBlocks").lazy.controller("TrainingCommentsController",["$scope",function($scope){
-    var me = this;
-    var config = $scope.blockConfig;
-    console.log(config);
-}]);
+angular.module("rubedoBlocks").lazy.controller("TrainingCommentsController",["$scope","CommentsService",
+    function($scope,CommentsService){
+        var me = this;
+        var config = $scope.blockConfig;
+        CommentsService.getComments().then(
+            function(response){
+                me.comments=response.data.comments;
+            },
+            function(response){
+
+            }
+
+        );
+    }]);
