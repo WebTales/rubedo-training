71ce63fc6a1676605352273c326fee8695b55e69
Alexandru Dobre <alexandru.dobre@webtales.fr>
2015-07-23 15:47:06 +0200

    bo

---
+++ app-extension/rubedotraining/store/TrainingComments.js
@@ -0,0 +1,42 @@
+Ext.define('Rubedo.store.TrainingComments', {
+    extend: 'Ext.data.Store',
+    alias: 'store.TrainingComments',
+
+    requires: [
+        'Rubedo.model.TrainingComment',
+        'Ext.data.proxy.Ajax',
+        'Ext.data.reader.Json',
+        'Ext.data.writer.Json'
+    ],
+
+    constructor: function(cfg) {
+        var me = this;
+        cfg = cfg || {};
+        me.callParent([Ext.apply({
+            autoLoad: true,
+            autoSync: true,
+            model: 'Rubedo.model.TrainingComment',
+            storeId: 'TrainingComments',
+            pageSize: 10000,
+            proxy: {
+                type: 'ajax',
+                api: {
+                    create: 'training-comments/create',
+                    read: 'training-comments',
+                    update: 'training-comments/update',
+                    destroy: 'training-comments/delete'
+                },
+                reader: {
+                    type: 'json',
+                    messageProperty: 'message',
+                    root: 'data'
+                },
+                writer: {
+                    type: 'json',
+                    encode: true,
+                    root: 'data'
+                }
+            }
+        }, cfg)]);
+    }
+});
---
+++ app-extension/rubedotraining/view/TrainingCommentsInterface.js
@@ -0,0 +1,61 @@
+Ext.define('Rubedo.view.TrainingCommentsInterface', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.TrainingCommentsInterface',
+
+
+    height: 456,
+    id: 'TrainingCommentsInterface',
+    width: 930,
+    constrainHeader: true,
+    iconCls: 'process-icon',
+    title: 'Comments',
+
+    layout: {
+        type: 'hbox',
+        align: 'stretch'
+    },
+
+    initComponent: function() {
+        var me = this;
+
+        Ext.applyIf(me, {
+
+            items:[
+                {
+                    xtype:"gridpanel",
+                    dockedItems:[
+                        {
+                            xtype: 'toolbar',
+                            dock: 'top',
+                            items: [{
+                                xtype: 'button',
+                                text: 'Delete',
+                                iconCls:"close",
+                                handler:function(button){
+                                    var comment=button.up().up().getSelectionModel().getLastSelected();
+                                    if(comment){
+                                        button.up().up().getStore().remove(comment);
+                                    }
+                                }
+                            }
+                            ]
+                        }],
+                    flex:1,
+                    store:"TrainingComments",
+                    columns: [
+                        {xtype: 'rownumberer'},
+                        { text: 'Comment',  dataIndex: 'comment',flex:3 },
+                        { text: 'Author',  dataIndex: 'createUser',flex:1 }
+                    ]
+                }
+            ]
+
+        });
+
+
+        me.callParent(arguments);
+    }
+
+
+
+});
